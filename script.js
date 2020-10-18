ef3f39a654c7630d627dfd24aa8c701e24c5f176
MalbonaGenio <pvalverdemolina@gmail.com>
2020-10-18 05:58:21 +0000

    Created the move fucntion where the snake is able to move to the next value of the squares array. Also set a setinterval to make the function move execute every second.

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const startButton = document.getElementById('start')
 const score = document.getElementById('score')
 
 let squares = []
-let snakePosition = [13,14,15]
+let snakePosition = [15,14,13]
+let direction = 1 // sets the direction where the snake is moving.
 
 function createGrid() {
   for (let i = 0; i < 100; i++){
@@ -16,6 +17,19 @@ function createGrid() {
 
 createGrid()
 
-// we change the style on the squares array on the indexes specified by snake position.
+// change the style on the squares array on the indexes specified by snake position.
 snakePosition.forEach(index => squares[index].classList.add("snake"))
+
+function move() {
+  //remove the tail of the snake and then we store it to remove the styling of that square
+  const tailPosition = snakePosition.pop() 
+  squares[tailPosition].classList.remove('snake')
+  //add the head of the snake on the first index of the snakePosition array. Then style that number in the squares array.
+  snakePosition.unshift(snakePosition[0] + direction)
+  squares[snakePosition[0]].classList.add('snake')
+}
+
+move()
+
+const timer = setInterval(move, 1000)
 
